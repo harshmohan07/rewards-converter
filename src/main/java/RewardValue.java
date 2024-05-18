bf5c40804f43fef1c304f2001d350eec8a17cad5
harshmohan07 <code.harshmohan@gmail.com>
2024-05-18 13:14:34 +0530

    Final Code for the RewardValue Class Added

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class RewardValue {
+public class RewardValue {
+    private final double cashValue;
+    private final int milesValue;
+
+    public RewardValue(double cv) {
+        cashValue = cv;
+        milesValue = (int)(cashValue/0.0035);
+    }
+
+    public RewardValue(int mv) {
+        milesValue = mv;
+        cashValue = mv*0.0035;
+    }
+
+    public double getCashValue() {
+        return cashValue;
+    }
+
+    public int getMilesValue() {
+        return milesValue;
+    }
+
 }
